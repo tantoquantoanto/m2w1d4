d9e52e361edc6208f6aec0229373b798aa63c97e
tantoquantoanto <168585464+tantoquantoanto@users.noreply.github.com>
2024-05-20 18:45:38 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+/* 
+I principali data type sono: 
+string : si tratta di una sequenza di caratteri e si dichiara con "" oppure '' o ancora con backtick ,
+number : si tratta di numeri che possono essere positivi, negativi, interi o digitali ;
+boolean : si tratta di un tipo di dato che può assumere solo due valori, true o false e viene utilizzato per confronti logici;
+null :  si tratta di un dato con il quale dichiariamo che una determinata variabile non ha valore;
+undefined : si tratta di un dato con il quale dichiariamo  che il valore della variabile che abbiamo introdotto non è definito ed il risultato in console sarà undefined sia che la definiamo undefined che se non gli assegniamo un valore
+ci sono inoltre due structural type: 
+oggetti: che si dichiarano con {} e contengono coppie chiave-valore, servono a descrivere una realtà
+array: sono liste che possono contenere tutti gli altri tipi di dati già menzionati e si dichiarano con []
+*/
+
+/* ESERCIZIO 2
+ Descrivi cos'è un oggetto in JavaScript, con parole tue.
+*/ 
+
+/*
+Un oggetto in javascript si dichiara con {}, contiene proprietà chiave-valore, serve a rappresentare una realtà, che sia una persona, un prodotto ecc e può contenere numeri, stringhe, altri oggetti o liste.
+
+let x = {
+    name : "Antonietta",
+    age : 32,
+    hobby : "singing"
+} 
+
+*/
+
+/* ESERCIZIO 3
+ Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+/* 
+let x = 12;
+let y= 20;
+let sum = x + y;
+console.log(sum)
+*/
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+/* let x = 12; */
+
+/* ESERCIZIO 5
+ Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+/* 
+const name = "Antonietta"
+*/
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+
+/* 
+let diff = (4 - x)
+console.log(diff) */
+
+/* ESERCIZIO 7
+*/
+
+/* 
+    const name1 = "john";
+    const name2 = "John";
+    console.log(name1 === name2)
+    console.log(name1.toLowerCase() === name2.toLowerCase());
+*/
+
+
+
